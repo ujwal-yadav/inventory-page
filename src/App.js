3ed793527e56306657bb4344e523b409f05f8920
ujwal-yadav <ujwalofficial99@gmail.com>
2024-07-13 18:07:15 +0530

    feat: set up the root file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@mui/material';
+import { Theme } from './theme';
+import Login from './containers/Login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Dashboard } from './containers/Dashboard/Dashboard';
+import AuthProvider from './context/AuthProvider';
+import ProtectedRoute from './layouts/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path="/" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
