14e8b019f43b3df9cd39b40d7dbd3db862a6cd73
ujwal-yadav <ujwalofficial99@gmail.com>
2024-07-14 11:43:32 +0530

    refactor: add edit and delete feat for products

--- src/components/InventoryTable/InventoryTable.hooks.js
+++ src/components/InventoryTable/InventoryTable.hooks.js
@@ -5,14 +5,51 @@ import { useEffect, useState } from 'react';
 export const useInventoryTable = () => {
   const [inventoryData, setInventoryData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [activeProductId, setActiveProductId] = useState(null);
+  const [errors, setErrors] = useState({
+    name: false,
+    price: false,
+    brand: false,
+    category: false,
+    stock: false,
+  });
+
+  const [product, setProduct] = useState({
+    name: '',
+    price: '',
+    brand: '',
+    category: '',
+    stock: '',
+  });
+
+  const validateField = (name, value) => {
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: !value,
+    }));
+
+    return !value;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value,
+    }));
+    validateField(name, value);
+  };
 
   const { token } = useAuth();
 
+  const headers = {
+    token,
+  };
+
   const getProducts = async () => {
     setIsLoading(true);
-    const headers = {
-      token,
-    };
 
     try {
       const response = await axios.get('/products', { headers });
@@ -21,7 +58,7 @@ export const useInventoryTable = () => {
         setInventoryData(response.data.data);
       }
     } catch (err) {
-      alert('Erorr while fetching inventory details!');
+      alert('Error while fetching inventory details!');
     } finally {
       setIsLoading(false);
     }
@@ -31,5 +68,89 @@ export const useInventoryTable = () => {
     getProducts();
   }, []);
 
-  return { isLoading, inventoryData };
+  const toggleEditProductModal = (id) => {
+    setActiveProductId(id);
+    setIsEditModalOpen(!isEditModalOpen);
+    if (!isEditModalOpen && inventoryData) {
+      const productData = inventoryData.find((product) => product.id === id);
+      setProduct({
+        name: productData.name,
+        price: productData.price,
+        brand: productData.brand.code,
+        category: productData.product_category.id,
+        stock: productData.minimum_stock,
+      });
+    }
+  };
+
+  const toggleDeleteProductModal = (id) => {
+    setActiveProductId(id);
+    setIsDeleteModalOpen(!isDeleteModalOpen);
+  };
+
+  const deleteProduct = async () => {
+    setIsDeleteModalOpen(false);
+    try {
+      const response = await axios.delete(`/products/${activeProductId}`, {
+        headers,
+      });
+
+      if (response.data === '') {
+        getProducts();
+      }
+    } catch (err) {
+      alert('Could not delete product!');
+    }
+  };
+
+  const editProduct = async () => {
+    const { name, price, brand, category, stock } = product;
+    const nameError = validateField('name', name);
+    const priceError = validateField('price', price);
+    const brandError = validateField('brand', brand);
+    const categoryError = validateField('category', category);
+    const stockError = validateField('stock', stock);
+
+    if (nameError || priceError || brandError || categoryError || stockError) {
+      return;
+    }
+
+    try {
+      const response = await axios.put(
+        `/products/${activeProductId}`,
+        {
+          id: activeProductId,
+          name,
+          price: Number(price),
+          brand: String(brand),
+          product_category: String(category),
+          minimum_stock: String(stock),
+        },
+        { headers }
+      );
+
+      if (response.data) {
+        setIsEditModalOpen(false);
+        getProducts();
+      }
+    } catch (err) {
+      alert('Could not update product!');
+    }
+  };
+
+  return {
+    isLoading,
+    inventoryData,
+    isEditModalOpen,
+    isDeleteModalOpen,
+    toggleEditProductModal,
+    toggleDeleteProductModal,
+    deleteProduct,
+    editProduct,
+    activeProductId,
+    errors,
+    validateField,
+    handleChange,
+    ...product,
+  };
 };
--- src/components/InventoryTable/InventoryTable.jsx
+++ src/components/InventoryTable/InventoryTable.jsx
@@ -7,11 +7,42 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Modal,
+  TextField,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
 import { useInventoryTable } from './InventoryTable.hooks';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import { useTheme } from '@emotion/react';
 
 export default function InventoryTable() {
-  const { isLoading, inventoryData } = useInventoryTable();
+  const theme = useTheme();
+  const isLargeView = useMediaQuery(theme.breakpoints.up('md'));
+
+  const {
+    isLoading,
+    inventoryData,
+    isEditModalOpen,
+    isDeleteModalOpen,
+    toggleEditProductModal,
+    toggleDeleteProductModal,
+    deleteProduct,
+    editProduct,
+    errors,
+    handleChange,
+    name,
+    price,
+    brand,
+    category,
+    stock,
+  } = useInventoryTable();
 
   if (isLoading)
     return (
@@ -29,6 +60,192 @@ export default function InventoryTable() {
 
   return (
     <>
+      <Modal open={isDeleteModalOpen} onClose={toggleDeleteProductModal}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: isLargeView ? 400 : '85%',
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 3,
+            borderRadius: 2,
+          }}
+        >
+          <Typography variant="h5" align="center" fontWeight={'600'}>
+            Delete Product
+          </Typography>
+          <IconButton
+            sx={{ position: 'absolute', top: 0, right: 0, margin: 2 }}
+            onClick={toggleDeleteProductModal}
+            size="small"
+          >
+            <ClearIcon />
+          </IconButton>
+
+          <Typography
+            variant="h6"
+            align="center"
+            fontWeight={'400'}
+            sx={{ paddingTop: 2 }}
+            color={'error'}
+          >
+            Are you sure you want to delete the product ?
+          </Typography>
+          <Button
+            sx={{
+              boxShadow: 0,
+              borderRadius: 2,
+              fontWeight: 600,
+              marginTop: 2,
+            }}
+            variant="contained"
+            color="error"
+            fullWidth
+            onClick={deleteProduct}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Modal>
+
+      <Modal open={isEditModalOpen} onClose={toggleEditProductModal}>
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: isLargeView ? 400 : '85%',
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 3,
+            borderRadius: 2,
+          }}
+        >
+          <Typography
+            variant="h5"
+            align="center"
+            fontWeight={'600'}
+            sx={{ paddingBottom: 1 }}
+          >
+            Edit Product
+          </Typography>
+          <IconButton
+            sx={{ position: 'absolute', top: 0, right: 0, margin: 2 }}
+            onClick={toggleEditProductModal}
+            size="small"
+          >
+            <ClearIcon />
+          </IconButton>
+
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Product Name"
+            name="name"
+            autoFocus
+            error={errors.name}
+            helperText={errors.name ? 'Product name is required' : ''}
+            value={name}
+            onChange={handleChange}
+            InputProps={{
+              style: {
+                borderRadius: '8px',
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="price"
+            label="Product Price"
+            id="price"
+            type="number"
+            error={errors.price}
+            helperText={errors.price ? 'Product price is required' : ''}
+            value={price}
+            onChange={handleChange}
+            InputProps={{
+              style: {
+                borderRadius: '8px',
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="brand"
+            label="Brand"
+            id="brand"
+            error={errors.brand}
+            helperText={errors.brand ? 'Brand is required' : ''}
+            value={brand}
+            onChange={handleChange}
+            InputProps={{
+              style: {
+                borderRadius: '8px',
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="category"
+            label="category"
+            id="category"
+            error={errors.category}
+            helperText={errors.category ? 'Product Category is required' : ''}
+            value={category}
+            onChange={handleChange}
+            InputProps={{
+              style: {
+                borderRadius: '8px',
+              },
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="stock"
+            label="Stock"
+            id="stock"
+            error={errors.stock}
+            helperText={errors.stock ? 'Stock is required' : ''}
+            value={stock}
+            onChange={handleChange}
+            InputProps={{
+              style: {
+                borderRadius: '8px',
+              },
+            }}
+          />
+
+          <Button
+            sx={{
+              boxShadow: 0,
+              borderRadius: 2,
+              fontWeight: 600,
+              marginTop: 2,
+            }}
+            variant="contained"
+            color="info"
+            fullWidth
+            onClick={editProduct}
+          >
+            Edit
+          </Button>
+        </Box>
+      </Modal>
+
       <Typography
         variant="h5"
         component="h1"
@@ -47,11 +264,20 @@ export default function InventoryTable() {
               <StyledTableCell>Product Category</StyledTableCell>
               <StyledTableCell>Stock</StyledTableCell>
               <StyledTableCell>Price</StyledTableCell>
+              <StyledTableCell
+                sx={{
+                  position: 'sticky',
+                  right: 0,
+                  top: 0,
+                  background: 'white',
+                  justifyContent: 'center',
+                }}
+              ></StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {inventoryData?.map((row) => (
-              <StyledTableRow key={row.name}>
+              <StyledTableRow key={row.id}>
                 <StyledTableCell>{row.brand.code}</StyledTableCell>
                 <StyledTableCell>{row.brand.name}</StyledTableCell>
                 <StyledTableCell>{row.code}</StyledTableCell>
@@ -59,6 +285,24 @@ export default function InventoryTable() {
                 <StyledTableCell>{row.product_category.name}</StyledTableCell>
                 <StyledTableCell>{row.minimum_stock}</StyledTableCell>
                 <StyledTableCell>₹{row.price}</StyledTableCell>
+                <StyledTableCell
+                  sx={{
+                    position: 'sticky',
+                    right: 0,
+                    background: 'white',
+                    justifyContent: 'center',
+                    display: 'flex',
+                    gap: 2,
+                    padding: '20px 25px',
+                  }}
+                >
+                  <IconButton onClick={() => toggleEditProductModal(row.id)}>
+                    <ModeEditIcon color="info" />
+                  </IconButton>
+                  <IconButton onClick={() => toggleDeleteProductModal(row.id)}>
+                    <DeleteIcon color="error" />
+                  </IconButton>
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
@@ -90,7 +334,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.body}`]: {
     [theme.breakpoints.up('md')]: {
       fontSize: 20,
-      padding: 20,
+      padding: '20px 25px',
     },
   },
 }));
